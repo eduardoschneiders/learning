35c4ec90d078dd5db7fe51e76e3d2750bb7b6a5a
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2013-06-18 15:37:27 -0300

    usando funções para ler arquivo

--- grep.py
+++ grep.py
@@ -4,13 +4,32 @@
 
 import sys
 #print sys.argv
+FOUND = 0
+NOT_FOUND = 1
+ERROR = 2
 
-search = sys.argv[1]
-filenames = sys.argv[2:]
+def find(search, filename):
 
-for filename in filenames:
   f = open(filename)
   for line in f:
     if search in line:
-      print "%s: %s" % (filename, line.strip())
+      yield line.strip()  
   f.close
+
+def main(args):
+
+  search = args[1]
+  filenames = args[2:]
+
+  ret = NOT_FOUND
+
+  for filename in filenames:
+    for line in find(search, filename):
+      print "%s:%s" % (filename, line)
+      ret = FOUND
+
+  return ret
+
+args = sys.argv
+ret = main(args)
+sys.exit(ret)
