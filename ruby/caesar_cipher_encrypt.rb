d2d04635145cd161561464863b4db613d7e8a586
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-06-11 15:53:56 -0300

    Change example

--- ruby/caesar_cipher_encrypt.rb
+++ ruby/caesar_cipher_encrypt.rb
@@ -56,7 +56,7 @@ def caesar_cipher (string, number, encrypt = true)
   encrypt_string
 end
 
-encrypted_string = caesar_cipher('O @zé ta bolado hoje', 5)
+encrypted_string = caesar_cipher('Eduardo Schneiders - noreply@gmail.com', 5)
 decrypted_string = caesar_cipher(encrypted_string, 5, false)
 
 puts "Encrypted: #{encrypted_string}"
