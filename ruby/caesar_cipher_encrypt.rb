ba5287195f5fea64c25d68b6c2c2427cc3db6c03
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-06-11 17:25:34 -0300

    Improve method to convert elements

--- ruby/caesar_cipher_encrypt.rb
+++ ruby/caesar_cipher_encrypt.rb
@@ -1,64 +1,11 @@
-require 'unicode_utils/upcase'
-
-class String
-  def upcase
-    UnicodeUtils.upcase(self)
-  end
-end
-
-def alphabet_down_case
-  ('a'..'z').to_a
-end
-
-def alphabet_up_case
-  ('A'..'Z').to_a
-end
-
-def alphabet_especial_caracters
-  [' ', '!', '@', '#', '$', '%', '*', '(', ')', '_', '-', '+', '=', '[', '{', '}', ']', '/', '?', ';', ':', '.', '>', ',', '<']
-end
-
-def alphabet_accent_caracters_down_case
-  ['à', 'á', 'â', 'ã', 'ä', 'è', 'é', 'ê', 'ẽ', 'ë', 'ì', 'í', 'î', 'ĩ', 'ï', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ũ', 'ü', 'ç']
-end
-
-def alphabet_accent_caracters_up_case
-  alphabet_accent_caracters_down_case.map(&:upcase)
-end
-
-def which_alphabet(letter)
-  if alphabet_down_case.include?(letter)
-    alphabet = alphabet_down_case
-  elsif alphabet_especial_caracters.include?(letter)
-    alphabet = alphabet_especial_caracters
-  elsif alphabet_accent_caracters_down_case.include?(letter)
-    alphabet = alphabet_accent_caracters_down_case
-  elsif alphabet_accent_caracters_up_case.include?(letter)
-    alphabet = alphabet_accent_caracters_up_case
-  else
-    alphabet = alphabet_up_case
-  end
-
-  alphabet
-end
-
 def caesar_cipher(string, number, encrypt = true)
   encrypt_string = ""
 
   string.split('').each do |letter|
-    alphabet = which_alphabet(letter)
-
-    alphabet.each_with_index do |value, index|
-      if letter == value
-        index = encrypt ? index - number : index + number
-        if index >= alphabet.length
-          index = index - alphabet.length
-        end
-        encrypt_string << alphabet[index]
-      end
-    end
+    ord_letter = letter.ord
+    index = encrypt ? ord_letter - number : ord_letter + number
+    encrypt_string << (index).chr
   end
-
   encrypt_string
 end
 
