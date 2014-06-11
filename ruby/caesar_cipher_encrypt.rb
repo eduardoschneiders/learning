e92878e7bafff234e9c1dbcd98fdbd4cfb65d2f0
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-06-11 13:02:40 -0300

    Add downcase, upcase and special caracters to convert

--- ruby/caesar_cipher_encrypt.rb
+++ ruby/caesar_cipher_encrypt.rb
@@ -1,11 +1,26 @@
-def alphabet
-  ['a', 'b', 'c', 'd', 'e', 'f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','y','z']
+def alphabet_down_case
+  ('a'..'z').to_a.push(alphabet_especial_caracters).flatten
+end
+
+def alphabet_up_case
+  ('A'..'Z').to_a.push(alphabet_especial_caracters).flatten
+end
+
+def alphabet_especial_caracters
+  [' ', '!', '@', '#', '$', '%', '*', '(', ')', '_', '-', '+', '=', '[', '{', '}', ']', '/', '?', ';', ':', '.', '>', ',', '<']
 end
 
 def caesar_cipher (string, number)
   encrypt_string = ""
 
   string.split('').each do |letter|
+
+    if alphabet_down_case.include?(letter)
+      alphabet = alphabet_down_case
+    else
+      alphabet = alphabet_up_case
+    end
+
     alphabet.each_with_index do |value, index|
       if letter == value
         encrypt_string << alphabet[index - number]
@@ -16,4 +31,4 @@ def caesar_cipher (string, number)
   encrypt_string
 end
 
-puts caesar_cipher('eduardo', 5)
+puts caesar_cipher('Eduardo Schneiders', 5)
