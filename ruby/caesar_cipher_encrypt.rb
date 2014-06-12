26fe95974bc062265e8e6fc47f6789c8371c82fc
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-06-12 10:32:07 -0300

    UTF-8 character support

--- ruby/caesar_cipher_encrypt.rb
+++ ruby/caesar_cipher_encrypt.rb
@@ -2,7 +2,7 @@ def caesar_cipher(string, number, encrypt = true)
   encrypt_string = string.each_char.map do |letter|
     ord_letter = letter.ord
     index = encrypt ? ord_letter - number : ord_letter + number
-    index.chr
+    index.chr('UTF-8')
   end.join
 
   encrypt_string
