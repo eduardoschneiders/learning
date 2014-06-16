47e840b581445fc0210da4e03c11ad7fa27d1789
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-06-16 18:07:48 -0300

    Typo

--- ruby/stock_picker.rb
+++ ruby/stock_picker.rb
@@ -29,7 +29,7 @@ def stock_picker(prices)
   better_days
 end
 
-better_days = stock_picker([17,3,6,9,15,8,6,1,10])
+better_days = stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
 
 puts "Better day to buy: #{better_days.first}"
 puts "Better day to sell: #{better_days.last}"
