3021f3f1705d5d826e950b54b1adc91eb3fd5003
Eduado Schneiders <eduado.schneiders@cwi.com.br>
2017-08-16 10:42:17 -0300

    Refactory

--- ruby/AI/8_puzzle/game.rb
+++ ruby/AI/8_puzzle/game.rb
@@ -7,33 +7,44 @@ require 'pry'
 # [7 8 _]
 
 class Table
-  attr_accessor :size, :lines, :table
+  attr_accessor :size, :original_matrix, :expected_matrix
 
-  def initialize(size, mixed, expected)
-    @lines = mixed
-    @expected_lines = expected
-    @size = size
+  def initialize(original_matrix, expected_matrix)
+    @original_matrix = original_matrix
+    @expected_matrix = expected_matrix
+    @size = original_matrix.size
   end
 
-  def sum_diff
-    sum = 0
-    (0..@size-1).each do |line|
-      (0..@size-1).each do |collumn|
-        if item = @expected_lines[line][collumn]
+  def score
+    score = 0
+
+    # expected_matrix.each do |expected_line|
+    #   expected_line.each do |expected_collumn|
+    #     found_line, found_collumn = find_position(expected_collumn)
+    #     diff = (found_line - line).abs + (found_collumn - collumn).abs
+    #     score += diff
+    #   end
+    # end
+
+    # score
+
+    (0..size-1).each do |expected_line|
+      (0..size-1).each do |expected_collumn|
+        if item = expected_matrix[expected_line][expected_collumn]
           found_line, found_collumn = find_position(item)
-          diff = (found_line - line).abs + (found_collumn - collumn).abs
-          sum += diff
+          diff = (found_line - expected_line).abs + (found_collumn - expected_collumn).abs
+          score += diff
         end
       end
     end
 
-    sum
+    score
   end
 
   def find_position(item)
     (0..@size-1).each do |line|
       (0..@size-1).each do |collumn|
-        if lines[line][collumn] == item
+        if original_matrix[line][collumn] == item
           return [line, collumn]
         end
       end
@@ -48,8 +59,8 @@ class Table
     line, collumn = find_blank_position
 
     if (line - 1 >= 0)
-      lines[line][collumn] = lines[line - 1][collumn]
-      lines[line - 1][collumn] = nil
+      original_matrix[line][collumn] = original_matrix[line - 1][collumn]
+      original_matrix[line - 1][collumn] = nil
     end
   end
 
@@ -57,8 +68,8 @@ class Table
     line, collumn = find_blank_position
 
     if (line + 1 < size)
-      lines[line][collumn] = lines[line + 1][collumn]
-      lines[line + 1][collumn] = nil
+      original_matrix[line][collumn] = original_matrix[line + 1][collumn]
+      original_matrix[line + 1][collumn] = nil
     end
   end
 
@@ -66,8 +77,8 @@ class Table
     line, collumn = find_blank_position
 
     if (collumn + 1 < size)
-      lines[line][collumn] = lines[line][collumn + 1]
-      lines[line][collumn + 1] = nil
+      original_matrix[line][collumn] = original_matrix[line][collumn + 1]
+      original_matrix[line][collumn + 1] = nil
     end
   end
 
@@ -75,8 +86,8 @@ class Table
     line, collumn = find_blank_position
 
     if (collumn - 1 >= 0)
-      lines[line][collumn] = lines[line][collumn - 1]
-      lines[line][collumn - 1] = nil
+      original_matrix[line][collumn] = original_matrix[line][collumn - 1]
+      original_matrix[line][collumn - 1] = nil
     end
   end
 
@@ -87,56 +98,44 @@ class Table
 
   def draw(action = 'initialization')
     puts "\n-------> #{action}"
-    lines.each { |line| p line }
+    original_matrix.each { |line| p line }
+  end
+
+  def duplicate
+    new(Marshal::load(Marshal.dump(original_matrix)), expected_matrix)
   end
 end
 
-expected = [[1,2,3],[4,5,6],[7,8,nil]]
-mixed = [
-  [nil,2,3],
-  [1,5,6],
-  [4,7,8]
-]
-expected = [
+expected_matrix = [
   [1,2,3],
   [8,nil,4],
   [7,6,5]
 ]
-mixed = [
+original_matrix = [
   [2,8,3],
   [1,6,4],
   [7,nil,5]
 ]
-# mixed = [[4,7,8],[1,5,3],[6,2,nil]]
 
-table = Table.new(3, mixed, expected)
-diff = table.sum_diff
+table = Table.new(original_matrix, expected_matrix)
+score = table.score
 
-while (diff != 0) do
+while (score != 0) do
   selected_action = nil
 
-  possible_actions = [:up, :right, :down, :left]
-  possible_actions.each do |action|
-    lines = Marshal::load(Marshal.dump(table.lines))
-    t = Table.new(3, lines, expected)
-    t.move(action)
-    before_sum ||= t.sum_diff
+  [:up, :right, :down, :left].each do |action|
+    current_score = score
+    new_table = table.duplicate
+    new_table.move(action)
 
-    if t.sum_diff < before_sum || t.sum_diff < table.sum_diff
-      before_sum = t.sum_diff
+    if new_table.score < current_score
+      current_score = new_table.score
       selected_action = action
     end
   end
-  if selected_action
-    table.move(selected_action, true) 
-  else
-    line, collumn =  table.find_blank_position()
-    actions = possible_actions
-    actions[2] = nil if line == 0
-    actions[0] if line == 2
-    actions[1] if collumn == 0
-    actions[3] if collumn == 2
-    table.move(actions.compact.sample, true)
-  end
-  diff = table.sum_diff
+
+  table.move(selected_action, true)
+  score = table.score
 end
+
+p 'Ended'
