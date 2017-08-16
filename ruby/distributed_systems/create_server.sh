6a6c3ace9fc7d7965cef9aa891169fec236ccfb7
Eduado Schneiders <eduado.schneiders@cwi.com.br>
2017-08-16 10:44:34 -0300

    Distributed system

--- ruby/distributed_systems/create_server.sh
+++ ruby/distributed_systems/create_server.sh
@@ -7,3 +7,13 @@ ln distributed_system//config/initializers/remote_control.rb distributed_system_
 
 rm distributed_system_$1/lib/middlewares/remote_control.rb
 ln distributed_system/lib/middlewares/remote_control.rb distributed_system_$1/lib/middlewares/
+
+rm distributed_system_$1/config/routes.rb
+ln distributed_system/config/routes.rb distributed_system_$1/config/routes.rb
+
+rm -rf distributed_system_$1/app/*
+ln -rs distributed_system/app/* distributed_system_$1/app/
+
+rm distributed_system_$1/Gemfile*
+ln  distributed_system/Gemfile* distributed_system_$1/
+
