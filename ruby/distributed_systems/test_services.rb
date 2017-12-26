767e98dcb034ee4dc23832aae739174a9999d7d4
eduardo.schneiders <eduardo.schneiders@cwi.com.br>
2017-12-26 09:35:26 -0200

    Improve tests and ensure servers are killed

--- ruby/distributed_systems/test_services.rb
+++ ruby/distributed_systems/test_services.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+require 'net/http'
+require 'json'
 
 def wait_until_get_pid(port)
   pid_command = "ps aux | grep '#{port}' | grep puma | grep -v grep | awk '{print $2}' "
@@ -29,28 +31,57 @@ end
 
 def expect!(received, expected)
   if received == expected
-    puts "--- TEST OK ---"
+    puts "\e[#{32}m--- TEST OK ---\e[0m"
   else
-    puts "NOT EQUAL. Expected #{expected}; Received #{received}"
+    puts "\e[#{31}m--------- NOT EQUAL. Expected #{expected}; Received #{received}\e[0m"
   end
 end
 
-create_server('3000', 1)
-pid1 = wait_until_get_pid('3000')
+def wait_for(time)
+  done = 0
+  time.to_i.times.each do
+    print("Waiting for #{time.to_i - done} ...\r")
+    done += 1
+    sleep(1)
+  end
+end
 
-create_server('3001', 2, false, '3000')
-pid2 = wait_until_get_pid('3001')
+def get(url)
+  Net::HTTP.get(URI(url))
+end
 
-p "Server started #{pid1}"
-p "Server started #{pid2}"
+begin
+  create_server('3000', 1)
+  pid1 = wait_until_get_pid('3000')
 
-hosts = File.open('distributed_system_1/hosts.csv', 'r') { |f| f.read }.split("\n")
-expect!(hosts, ["http://localhost:3001"])
+  create_server('3001', 2, false, '3000')
+  pid2 = wait_until_get_pid('3001')
 
-hosts = File.open('distributed_system_2/hosts.csv', 'r') { |f| f.read }.split("\n")
-expect!(hosts, ["http://localhost:3000"])
+  hosts = File.open('distributed_system_1/hosts.csv', 'r') { |f| f.read }.split("\n")
+  expect!(hosts, ["http://localhost:3001"])
 
-# sleep(30)
-kill_server(pid1, 1)
-kill_server(pid2, 2)
+  hosts = File.open('distributed_system_2/hosts.csv', 'r') { |f| f.read }.split("\n")
+  expect!(hosts, ["http://localhost:3000"])
+
+  get('http://localhost:3000/planes/-KzcocrWhEx8Fw2oivI0/try_to_reserve/6')
+  reserved_resources = JSON.parse(File.open('distributed_system_1/reserved_resources.csv', 'r') { |f| f.read })
+  expect!(reserved_resources['resource'], {"plane_id"=>"-KzcocrWhEx8Fw2oivI0", "plane_name"=>"Plane 04", "seat_number"=>"6"})
+
+
+  get('http://localhost:3001/planes/-KzcocrWhEx8Fw2oivI0/try_to_reserve/7')
+  reserved_resources = JSON.parse(File.open('distributed_system_2/reserved_resources.csv', 'r') { |f| f.read })
+  expect!(reserved_resources['resource'], {"plane_id"=>"-KzcocrWhEx8Fw2oivI0", "plane_name"=>"Plane 04", "seat_number"=>"7"})
+
+  wait_for(65)
+
+  reserved_resources = File.open('distributed_system_2/reserved_resources.csv', 'r') { |f| f.read }
+  expect!(reserved_resources, '')
+
+  reserved_resources = File.open('distributed_system_1/reserved_resources.csv', 'r') { |f| f.read }
+  expect!(reserved_resources, '')
+ensure
+  kill_server(pid1, 1)
+  kill_server(pid2, 2)
+  exit
+end
 
