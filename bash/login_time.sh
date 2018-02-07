144aa26ec9a398d59586dcbc1902a8d192aebff7
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2018-02-07 12:26:19 -0200

    Change regular expression

--- bash/login_time.sh
+++ bash/login_time.sh
@@ -1,17 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 # A simple bash script to parse auth file and check login time
 
 LANG=en_US
 TODAY=$(date +"%b %_d")
 FILE=/tmp/auth.log
-sudo cat /var/log/auth.log.1 /var/log/auth.log > $FILE
+# sudo cat /var/log/auth.log.1 /var/log/auth.log > $FILE
+sudo cat /var/log/auth.log > $FILE
 USER=$(whoami)
 RESULT=$(grep -E "$TODAY.*adclient.*INFO.*PAM authentication granted.*$USER" $FILE| grep -v 'COMMAND' | grep -v 'sudo')
 
-while read line; do
-  echo $line | awk '{print $3;}'
-done <<< "$RESULT"
+# while read line; do
+#   echo $line | awk '{print $3;}'
+# done <<< "$RESULT"
 
+# echo $RESULT
 IFS=$'\n' read -rd '' -a lines <<< "$RESULT"
 hour=$(echo ${lines[0]} | awk '{print $3;}')
 add=$(echo 8 0.5 | awk '{print ($1 + 1.0 + $2)*60 }')
