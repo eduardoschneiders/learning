334775b06aa5d00d1b432a35b3862488c540d62a
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2018-02-20 17:13:39 -0300

    Knn working with cars dataset

--- ruby/AI/knn/knn.rb
+++ ruby/AI/knn/knn.rb
@@ -1,19 +1,33 @@
-a = [4,4]
-c = [2,2]
+require 'json'
 
-train_data = [
-  [4,4, 'x'],
-  [7,7, 'x'],
-  [10,10, 'y'],
-  [15,15, 'y'],
-  [20,20, 'y'],
-  [30,35, 'y'],
-  [1,1, 'x'],
-  [3,3, 'x'],
+CAR_ATTRS = [
+  'symboling',
+  'normalized-losses',
+  'make',
+  'fuel-type',
+  'aspiration',
+  'num-of-doors',
+  'body-style',
+  'drive-wheels',
+  'engine-location',
+  'wheel-base',
+  'length',
+  'width',
+  'height',
+  'curb-weight',
+  'engine-type',
+  'num-of-cylinders',
+  'engine-size',
+  'fuel-system',
+  'bore',
+  'stroke',
+  'compression-ratio',
+  'horsepower',
+  'peak-rpm',
+  'city-mpg',
+  'highway-mpg',
+  'price'
 ]
-
-a = [2, 2, 'x']
-b = [20, 15, 'y']
 
 def euclidean_distance(instance1, instance2, k)
   total = 0
@@ -26,20 +40,72 @@ end
 
 def neighbors(train_data, instance)
   train_data.map do |instance_data|
-    distance = euclidean_distance(instance_data, instance, instance.size - 1)
-    { distance: distance, name: instance_data[-1] }
+    distance = euclidean_distance(instance_data[:data], instance[:data], instance[:data].size - 1)
+    { distance: distance, name: instance_data[:data][-1], instance: instance_data }
   end.sort_by do |data|
     data[:distance]
   end
 end
 
-def class_name(instances, k)
-  instances.take(k).inject({}) do |data, i|
-    data[i[:name]] ||= 0
-    data[i[:name]] += 1
-    data
-  end.keys.first
+def class_name(instances)
+  instances.group_by do |i|
+    i[:name]
+  end.map do |name, instances|
+    [
+      name,
+      instances.inject(0) { |total, i| i[:distance] + total } / instances.size
+    ]
+  end.sort_by { |a| a[1]}.first[0]
 end
 
-neighbors = neighbors(train_data, b)
-p class_name(neighbors, 3)
+def normalize_value(values, current)
+  current/values.max
+end
+
+def normalize_field(values, value)
+  values.group_by { |a| a }.keys.map { |a| a == value ? 1 : 0}
+end
+
+def json_data(file)
+  file = File.open(file, 'r') { |f| f.read}
+  JSON.parse(file)
+end
+
+def data
+  file_name = 'data_flags.json'
+  file_name = 'data_cars.json'
+
+  data = json_data(file_name)
+  data.select { |d| d.none? { |prop| prop == '?'} }
+end
+
+def car_attributes(data, record, attributes)
+  attributes.map do |attr|
+    index_attribute = CAR_ATTRS.index(attr)
+    raw_value = record[index_attribute]
+
+    if raw_value.class == String
+      normalize_field(data.map{ |d| d[index_attribute]}, raw_value)
+    else
+      normalize_value(data.map{ |d| d[index_attribute]}, raw_value)
+    end
+  end.flatten
+end
+
+train_data = data.map do |i|
+  index_attribute = CAR_ATTRS.index('fuel-type')
+  raw_value = i[index_attribute]
+
+  {
+    data: car_attributes(data, i, ['length', 'width', 'height', 'num-of-cylinders', 'num-of-doors', 'fuel-system']).push(raw_value),
+    raw: i
+  }
+end.compact
+
+instance = train_data[44]
+neighbors = neighbors(train_data, instance)
+name = class_name(neighbors)
+
+p "Predicted Class: #{name}"
+p "Actual Class: #{instance}"
+p "Most relevant: #{neighbors.first[:instance]}"
